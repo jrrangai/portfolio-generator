cd78cf165400f4ea9052648e30501e68fa39fe5f
Joshua Rangai <rangaij0116@hotmail.com>
2022-04-22 10:38:06 -0500

    9.2.6

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
-const profileDataArgs = process.argv.slice(2);
+const fs = require( 'fs' );
+const generatePage = require('./src/page-template');
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.htm to see the output!');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const printProfileData = profileDataArr => {
+//     // This...
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//       console.log(profileDataArr[i]);
+//     }
   
-    console.log('================');
+//     console.log('================');
   
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-  };
+//     // Is the same as this...
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+//   };
   
-  printProfileData(profileDataArgs);
+//   printProfileData(profileDataArgs);
