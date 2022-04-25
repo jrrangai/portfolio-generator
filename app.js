bdf47c673a34a918b5e52298b209a98324f45c35
Joshua Rangai <rangaij0116@hotmail.com>
2022-04-25 18:10:02 -0500

    multiple projects

--- app.js
+++ app.js
@@ -1,20 +1,96 @@
 const inquirer = require('inquirer');
-inquirer
-  .prompt([
+
+const promptUser = () => {
+  return inquirer.prompt([
     {
       type: 'input',
       name: 'name',
       message: 'What is your name?'
-    }
-  ])
-  .then(answers => console.log(answers))
-  .catch((error) => {
-    if (error.isTtyError) {
-      console.log('Prompt could not be rendered.')
-    } else {
-      console.log('Something else went wrong!')
-    }
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username'
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself:'
+    },
+  ]);
+};
+
+const promptProject = portfolioData => {
+  // If there are no project in array property, it will create empty array
+  if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  };
+  console.log (`
+  ========
+  Add a New Project
+  ========
+  `);
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of your project?'
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project (Required)'
+      },
+      {
+        type: 'checkbox',
+        name: 'languages',
+        message: 'What did you build this project with? (Check all that apply)',
+        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+      },
+      {
+        type: 'input',
+        name: 'link',
+        message: 'Enter the GitHub link to your project. (Required)'
+      },
+      {
+        type: 'confirm',
+        name: 'feature',
+        message: 'Would you like to feature this project?',
+        default: false
+      },
+      {
+        type: 'confirm',
+        name: 'confirmAddProject',
+        message: 'Would you like to enter another project?',
+        default: false
+      },
+    ])
+    .then (projectData => {
+      portfolioData.projects.push(projectData);
+      if (projectData.confirmAddProject) {
+        return promptProject(portfolioData);
+      } else {
+        return portfolioData;
+      }
+    });
+};
+
+promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    console.log(portfolioData);
   });
+  
+// .catch((error) => {
+//   if (error.isTtyError) {
+//     console.log('Prompt could not be rendered.')
+//   } else {
+//     console.log('Something else went wrong!')
+//   }
+// });
+
+
+
 
 // const fs = require( 'fs' );
 // const generatePage = require('./src/page-template');
@@ -41,17 +117,3 @@ inquirer
 
 
 
-
-// const printProfileData = profileDataArr => {
-//     // This...
-//     for (let i = 0; i < profileDataArr.length; i += 1) {
-//       console.log(profileDataArr[i]);
-//     }
-  
-//     console.log('================');
-  
-//     // Is the same as this...
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-//   };
-  
-//   printProfileData(profileDataArgs);
