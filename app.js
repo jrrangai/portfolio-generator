8e5178b454b7372a34ad2b29eeb6c4e4dc3c33dd
Joshua Rangai <rangaij0116@hotmail.com>
2022-04-25 21:32:34 -0500

    display about and projects

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const fs = require( 'fs' );
+const generatePage = require('./src/page-template');
+
 const inquirer = require('inquirer');
 
 const promptUser = () => {
@@ -131,7 +134,13 @@ const promptProject = portfolioData => {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
   });
   
 // .catch((error) => {
@@ -140,20 +149,6 @@ promptUser()
 //   } else {
 //     console.log('Something else went wrong!')
 //   }
-// });
-
-
-
-
-// const fs = require( 'fs' );
-// const generatePage = require('./src/page-template');
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.htm to see the output!');
 // });
 
 
